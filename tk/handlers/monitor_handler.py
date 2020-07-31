976342bc557546870a99c72ec3840e50ed180470
fengjp <366085674@qq.com>
2020-07-31 17:32:01 +0800

    1

--- tk/handlers/monitor_handler.py
+++ tk/handlers/monitor_handler.py
@@ -15,6 +15,7 @@ from settings import CUSTOM_DB_INFO
 from libs.server.server_common import *
 from libs.myAnsible import AnsiableAPI
 
+
 class MonitorHandler(BaseHandler):
     def get(self, *args, **kwargs):
         ipstr = self.get_argument('key', default=None)
@@ -23,13 +24,13 @@ class MonitorHandler(BaseHandler):
         ip_list = [ipstr]
         serverObjList = get_serverObjList(ip_list)
         asb = AnsiableAPI(connection='smart', hostsresource=serverObjList)
-        #获取卡片数据
+        # 获取卡片数据
         cardlist = []
         if cardtype == '0':
-            carddict = getcard0(ipstr,asb)#主机基本信息
+            carddict = getcard0(ipstr, asb)  # 主机基本信息
             cardlist.append(carddict)
         if cardtype == '1':
-            carddict = getcard1(ipstr,asb)#文件分区使用情况
+            carddict = getcard1(ipstr, asb)  # 文件分区使用情况
             cardlist.append(carddict)
         # asb.run(hosts=ipstr, module="shell", args='uptime')
         # stdout_dict = json.loads(asb.get_result())
@@ -37,21 +38,24 @@ class MonitorHandler(BaseHandler):
             self.write(dict(code=0, msg='获取報告成功', count=1, data=cardlist))
         else:
             self.write(dict(code=-1, msg='没有相关数据', count=0, data=[]))
-def getcard0(ipstr,asb):
+
+
+def getcard0(ipstr, asb):
     carddict = {"title": "主机基本信息", "flag": "0", "tableData": []}
-    data_dict  = {"系统IP":ipstr,"主机":ipstr,"CPU频率":"","物理内存总大小":"","CPU核数":"","操作系统版本":"","系统位数":"","维护人":"","维护人联系电话":""}
+    data_dict = {"系统IP": ipstr, "主机": ipstr, "CPU频率": "", "物理内存总大小": "", "CPU核数": "", "操作系统版本": "", "系统位数": "",
+                 "维护人": "", "维护人联系电话": ""}
     # cpu频率
     asb.run(hosts=ipstr, module="shell", args='lscpu')
     stdout_dict = json.loads(asb.get_result())
     if stdout_dict["success"]:
 
-        data_dict["CPU频率"] =  stdout_dict["success"][ipstr]["stdout_lines"][14].split()[2] + "MHz"
+        data_dict["CPU频率"] = stdout_dict["success"][ipstr]["stdout_lines"][14].split()[2] + "MHz"
         data_dict["CPU核数"] = stdout_dict["success"][ipstr]["stdout_lines"][3].split()[1]
         data_dict["系统位数"] = stdout_dict["success"][ipstr]["stdout_lines"][0].split()[1]
 
     else:
         data_dict["CPU频率"] = ""
-    #获取物理内存
+    # 获取物理内存
     asb.run(hosts=ipstr, module="shell", args='free')
     stdout_dict = json.loads(asb.get_result())
     if stdout_dict["success"]:
@@ -66,16 +70,18 @@ def getcard0(ipstr,asb):
     data_dict["操作系统版本"] = stdout_dict["failed"][ipstr]["stdout_lines"][0]
     carddict["tableData"].append(data_dict)
     return carddict
-def getcard1(ipstr,asb):
+
+
+def getcard1(ipstr, asb):
     carddict = {"title": "文件分区使用情况", "flag": "1", "tableData": []}
     # 文件分区使用情况
     asb.run(hosts=ipstr, module="shell", args='df -h')
     stdout_dict = json.loads(asb.get_result())
     if stdout_dict["success"]:
         temp_list = stdout_dict["success"][ipstr]["stdout_lines"]
-        for i in range(1,len(temp_list)):
+        for i in range(1, len(temp_list)):
             data_dict = {"name": "", "size": "", "utilization_rate": "", "capacity": "", "usedsize": ""}
-            data_dict["name"] = temp_list[i].split( )[5]
+            data_dict["name"] = temp_list[i].split()[5]
             data_dict["size"] = temp_list[i].split()[3]
             data_dict["utilization_rate"] = temp_list[i].split()[4]
             data_dict["capacity"] = temp_list[i].split()[1]
@@ -100,6 +106,8 @@ def getcard1(ipstr,asb):
     # ins_log.read_log('info', carddict)
     # ins_log.read_log('info', "1111111111111111112222222222222222222")
     return carddict
+
+
 monitor_urls = [
     (r"/v1/monitor/serInfo/", MonitorHandler),
 ]
